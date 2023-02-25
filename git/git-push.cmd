468c8d44f9a31ac60c660974384e8d4cd2cff3b0
Navneet <88278303+navneetguptacse@users.noreply.github.com>
2023-02-25 23:58:51 +0530

    Add files via upload

---
+++ git/git-push.cmd
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "my-commit"
+git branch -M main
+git remote remove origin
+git remote add origin git@github.com:navneetguptacse/Calculator-flask-api.git
+git push -u origin main
